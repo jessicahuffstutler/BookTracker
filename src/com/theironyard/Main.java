9b045f1c19aec2d282a9219cb050d9026349938a
jessicahuffstutler <jessica.huffstutler@gmail.com>
2015-11-01 22:32:20 -0500

    login, input, delete, edit display page

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -11,11 +11,11 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Book> books = new ArrayList();
+        HashMap<String, User> users = new HashMap<>();
 
         Spark.get(
                 "/",
-                ((request, response) -> {
+                (request, response) -> {
                     Session session = request.session();
                     String username = session.attribute("username");
                     String password = session.attribute("password");
@@ -25,21 +25,33 @@ public class Main {
                     HashMap m = new HashMap();
                     m.put("username", username);
                     m.put("password", password);
-                    m.put("books", books);
+                    m.put("books", users.get(username).books);
                     return new ModelAndView(m, "logged-in.html");
-                }),
+                },
                 new MustacheTemplateEngine()
         );
 
         Spark.post(
                 "/login",
                 ((request, response) -> {
+                    Session session = request.session();
                     String username = request.queryParams("username");
                     String password = request.queryParams("password");
-                    Session session = request.session();
                     session.attribute("username", username);
                     session.attribute("password", password);
-                    response.redirect("/");
+                    if (users.get(username) == null) {
+                        User currentUser = new User();
+                        currentUser.username = username;
+                        currentUser.password = password;
+                        users.put(username, currentUser);
+                        response.redirect("/");
+                    }
+                    else if (username.equals(users.get(username).username) && password.equals(users.get(username).password)) {
+                        response.redirect("/");
+                    }
+                    else {
+                        return "ERROR";
+                    }
                     return "";
                 })
         );
@@ -47,11 +59,8 @@ public class Main {
         Spark.post(
                 "/logout",
                 ((request, response) -> {
-                    String username = request.queryParams("username");
-                    String password = request.queryParams("password");
                     Session session = request.session();
-                    session.attribute("username", username);
-                    session.attribute("password", password);
+                    session.invalidate();
                     response.redirect("/");
                     return "";
                 })
@@ -60,13 +69,16 @@ public class Main {
         Spark.post(
                 "/input-books",
                 ((request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
                     Book book = new Book();
-                    book.id = books.size() + 1; //starts at 0, adding 1 to show human counting
+                    book.id = users.get(username).books.size() + 1; //starts at 0, adding 1 to show human counting
                     book.title = request.queryParams("title");
                     book.author = request.queryParams("author");
                     book.qty = request.queryParams("qty");
                     book.type = request.queryParams("type");
-                    books.add(book);
+                    users.get(username).books.add(book);
                     response.redirect("/");
                     return "";
                 })
@@ -75,11 +87,14 @@ public class Main {
         Spark.get(
                 "/book",
                 ((request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
                     HashMap m = new HashMap();
 
                     String id = request.queryParams("id");
                     int idNum = Integer.valueOf(id);
-                    Book book = books.get(idNum - 1);
+                    Book book = users.get(username).books.get(idNum - 1);
                     m.put("book", book);
 
                     return new ModelAndView(m, "book.html");
@@ -87,8 +102,78 @@ public class Main {
                 new MustacheTemplateEngine()
         );
 
+        Spark.get(
+                "/edit-book",
+                ((request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
 
+                    HashMap m = new HashMap();
+
+                    String id = request.queryParams("id");
+                    int idNum = Integer.valueOf(id);
+                    Book book = users.get(username).books.get(idNum - 1);
+                    m.put("book", book);
+
+                    return new ModelAndView(m, "edit-book.html");
+                }),
+                new MustacheTemplateEngine()
+        );
+
+        Spark.post(
+                "/edit-book",
+                ((request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
+                    String id = request.queryParams("id");
+                    String title = request.queryParams("title");
+                    String author = request.queryParams("author");
+                    String qty = request.queryParams("qty");
+                    String type = request.queryParams("type");
+                    try {
+                        int idNum = Integer.valueOf(id);
+                        ArrayList<Book> books = users.get(username).books;
+                        Book book = books.get(idNum - 1);
+                        book.title = title;
+                        book.author = author;
+                        book.qty = qty;
+                        book.type = type;
+                        for (int i = 0; i < users.get(username).books.size(); i++) {
+                            users.get(username).books.get(i).id = i + 1;
+                        }
+                    } catch (Exception e) {
+
+                    }
+                    response.redirect("/");
+                    return "";
+                })
+        );
+
+        Spark.post(
+                "/delete-book",
+                ((request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
+                    String id = request.queryParams("id");
+                    try {
+                        int idNum = Integer.valueOf(id);
+                        users.get(username).books.remove(idNum - 1);
+                        for (int i = 0; i < users.get(username).books.size(); i++) { //renumbering the books after you delete one of them
+                            users.get(username).books.get(i).id = i + 1; //renumbering the books after you delete one of them
+                        }
+                    } catch (Exception e) {
+
+                    }
+                    response.redirect("/");
+                    return "";
+                })
+        );
 
 
     }
 }
+
+//doesn't maintain login on the book & edit-book pages.
+//edit doesn't submit
--- src/com/theironyard/User.java
+++ src/com/theironyard/User.java
@@ -1,8 +1,13 @@
 package com.theironyard;
 
+import java.util.ArrayList;
+
 /**
  * Created by jessicahuffstutler on 10/29/15.
  */
 public class User {
+    String username;
     String password;
+
+    ArrayList<Book> books = new ArrayList<>();
 }
